b2ec1d1a7576fc81906c0d338c7f9228c4b68052
bplsmolders <79159158+bplsmolders@users.noreply.github.com>
2021-03-03 13:56:53 +0100

    added all the necessary functions to meet "exceeds"

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -18,6 +18,9 @@ const creditcardDiv = document.getElementById("credit-card");
 const paypalDiv = document.getElementById("paypal");
 const bitcoinDiv = document.getElementById("bitcoin");
 const submitButton = document.querySelector("button");
+const cardnumber = document.getElementById("cc-num");
+const zipcode = document.getElementById("zip");
+const cvv = document.getElementById("cvv");
 
 // sets 'name' field to focus
 nameInput.focus();
@@ -36,7 +39,6 @@ titleInput.addEventListener("change", e => {
 shirtColorsDiv.style.display='none';
 designInput.addEventListener("change", e => {
   colorInput.value = colorOptions[0]
-// display "Cornflower Blue," "Dark Slate Grey," and "Gold."
   if(designInput.value === "js puns"){
     shirtColorsDiv.style.display='block';
     colorOptions[1].style.display='block';
@@ -47,7 +49,6 @@ designInput.addEventListener("change", e => {
     colorOptions[5].style.display='none';
     colorOptions[6].style.display='none';
   }
-// display "Tomato," "Steel Blue," and "Dim Grey."
   if(designInput.value === "heart js"){
     shirtColorsDiv.style.display='block';
     colorOptions[1].style.display='none';
@@ -93,36 +94,112 @@ paymentOptions.addEventListener("change", e => {
   }
 });
 
-// form validation
+// function underneath checks form validation and adds visible errors where necessary
 form.addEventListener('submit', e => {
-  e.preventDefault();
-  if (/[a-z]+/.test(nameInput)){
-    nameInput.nextElementSibling.display ='block';
+  if (/^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameInput.value) !== true){
+    nameInput.nextElementSibling.style.display = "inherit";
+    nameInput.parentNode.className = 'not-valid';
+    e.preventDefault();
+  } else{
+    nameInput.nextElementSibling.style.display = "none";
+    nameInput.parentNode.className = 'valid';
+  }
+
+  if (/^[\w]+@[\w]+\.[\w]{3}$/.test(emailInput.value) !== true) {
+    emailInput.nextElementSibling.textContent = "Email address must be formatted correctly";
+    if(/^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(emailInput.value)!== true){
+      emailInput.nextElementSibling.textContent = "Email field cannot be blank";
+    }
+    emailInput.nextElementSibling.style.display = "inherit";
+    emailInput.parentNode.className = 'not-valid';
+    e.preventDefault();
+  } else {
+    emailInput.nextElementSibling.style.display = "none";
+    emailInput.parentNode.className = 'valid';
+  }
+
+  for(let i = 0; i<activitiesCheckboxes.length; i++){
+    if(activitiesCheckboxes[i].firstElementChild.checked){
+      document.getElementById("activities-hint").style.display = "none";
+      document.getElementById("activities").className = "activities valid";
+      break;
+    }else{
+      if(i === activitiesCheckboxes.length - 1) {e.preventDefault()};
+      document.getElementById("activities-hint").style.display = "inherit";
+      document.getElementById("activities").className = 'activities not-valid';
+    }
+  }
+
+  if(paymentOptions.value === "credit-card"){
+    if (/^\d{13,16}$/.test(cardnumber.value) !== true) {
+      cardnumber.nextElementSibling.style.display = "inherit";
+      cardnumber.parentNode.className = 'not-valid';
+      e.preventDefault();
+    } else {
+      cardnumber.nextElementSibling.style.display = "none";
+      cardnumber.parentNode.className = 'valid';
+    }
+    if (/^\d{5}$/.test(zipcode.value) !== true) {
+      zipcode.nextElementSibling.style.display = "inherit";
+      zipcode.parentNode.className = 'not-valid';
+      e.preventDefault();
+    } else {
+      zipcode.nextElementSibling.style.display = "none";
+      zipcode.parentNode.className = 'valid';
+    }
+    if (/^\d{3}$/.test(cvv.value) !== true) {
+      cvv.nextElementSibling.style.display = "inherit";
+      cvv.parentNode.className = 'not-valid';
+      e.preventDefault();
+    } else {
+      cvv.nextElementSibling.style.display = "none";
+      cvv.parentNode.className = 'valid';
+    }
   }
 });
 
-
-// checks the emailInput and gives real time feedback
-function validEmail (email){
-  return/^[\w]+@[\w]+\.[\w]{3}$/.test(email);
-};
-
-function showOrHideTip(show, element) {
-  if (show) {
-    element.style.display = "inherit";
-  } else {
-    element.style.display = "none";
-  }
+// the functions below gives the checkboxes an class when focuses for better accessibility
+for(let i =0; i<activitiesCheckboxes.length; i++){
+  activitiesCheckboxes[i].firstElementChild.addEventListener('focus', e =>{
+    e.target.parentNode.className = 'focus';
+  });
+  activitiesCheckboxes[i].firstElementChild.addEventListener('blur', e =>{
+    e.target.parentNode.className = "";
+  });
 }
 
-function createListener(validator) {
-  return e => {
-    const text = e.target.value;
-    const valid = validator(text);
-    const showTip = text !== "" && !valid;
-    const tooltip = e.target.nextElementSibling;
-    showOrHideTip(showTip, tooltip);
+// functions below checks the emailInput and gives real time feedback
+emailInput.addEventListener('keyup', e =>{
+  if(/^[\w]+@[\w]+\.[\w]{3}$/.test(emailInput.value) !== true){
+    emailInput.nextElementSibling.style.display = "inherit";
+  } else{
+    emailInput.nextElementSibling.style.display = "none"
   };
-}
+});
 
-emailInput.addEventListener("input", createListener(validEmail));
+// functions below disables overlapping activitiesDiv
+activitiesDiv.addEventListener('change', e=> {
+  if(activitiesCheckboxes[1].firstElementChild.checked){
+    activitiesCheckboxes[3].className = 'disabled'
+  } else {
+    activitiesCheckboxes[3].className = ''
+  }
+
+  if(activitiesCheckboxes[3].firstElementChild.checked){
+    activitiesCheckboxes[1].className = 'disabled'
+  } else{
+    activitiesCheckboxes[1].className = ''
+  }
+
+  if(activitiesCheckboxes[2].firstElementChild.checked){
+    activitiesCheckboxes[4].className = 'disabled'
+  } else {
+    activitiesCheckboxes[4].className = ''
+  };
+
+  if(activitiesCheckboxes[4].firstElementChild.checked){
+    activitiesCheckboxes[2].className = 'disabled'
+  } else {
+    activitiesCheckboxes[2].className = ''
+  }
+});
